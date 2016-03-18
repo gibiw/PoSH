4e39b9c3002dd1bac743ec7adae6ba6c4d7c9254
gibiw <dmitriy.gridnev@outlook.com>
2016-03-18 13:23:25 +0300

    Test

---
+++ Scripts/Get-adgroupmember.ps1
@@ -0,0 +1,10 @@
+﻿$group = get-adgroup -Filter {GroupCategory -eq "Security"} -SearchBase "ou=IS_OEM,ou=Groups,ou=DataCenterEM01,DC=passport,DC=local" | Select SamAccountName | Export-Csv C:\New_users\test.csv –Delimiter “;” -Encoding UTF8
+
+$2 = Import-Csv C:\New_users\test.csv –Delimiter “;”
+
+foreach ($gp in $2) 
+{
+$1 = $gp.SamAccountName
+
+ Get-ADGroupMember -Identity $1 -recursive | select Name | Export-csv C:\New_users\Groups\$1.csv -Encoding UTF8 } 
+
