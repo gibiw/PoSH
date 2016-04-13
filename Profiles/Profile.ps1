c825b082e6774057c05f57ea603c606849524469
Dmitriy Gridnev <Dmitriy Gridnev>
2016-04-13 09:08:13 +0300

    Version 1.6

--- Profiles/Profile.ps1
+++ Profiles/Profile.ps1
@@ -139,7 +139,7 @@ if (!(Test-Path -Path $profile.CurrentUserAllHosts)) {
     .$profile.CurrentUserAllHosts
     }
 else{
-    if(Compare-Object -ReferenceObject (Get-Content -Path ($PowerShellScriptsFolder+'\Profiles\Profile.ps1')) -DifferenceObject (Get-Content -Path ($PowerShellScriptsFolder+'\Profiles\Profile.ps1'))){
+ if((Compare-Object -ReferenceObject (Get-Content -Path ($PowerShellScriptsFolder+'\Profiles\Profile.ps1')) -DifferenceObject (Get-Content -Path ($PowerShellScriptsFolder+'\Profiles\Profile.ps1'))) -eq $null){
         Copy-Item -Path ($PowerShellScriptsFolder+'\Profiles\Profile.ps1') -Destination $profile.CurrentUserAllHosts -Force
         .$profile.CurrentUserAllHosts
         }
